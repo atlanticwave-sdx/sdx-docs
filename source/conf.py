ce5c7f0faefe9b39f0a4a11d15be157bb0486219
Luis Marin <lmarinve@gmail.com>
2026-02-06 17:01:39 +0000

    docs: exclude submodule from sphinx indexing

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,13 @@ extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = []
+
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "_external/**",
+]
 
 language = 'en'
 
